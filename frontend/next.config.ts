29bfdd22ea3636e6d90957237cfe7f12f71fa77a
miqnni <miqnni@protonmail.com>
2025-07-21 16:57:08 +0200

    Simple landing page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
-
-export default {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
-}
+};
+
+export default nextConfig;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,34 @@
+"use client"
+
 import Image from "next/image";
-import { Button, HStack } from "@chakra-ui/react"
+import { Button, HStack, Center, Stack, Text } from "@chakra-ui/react"
+import { MuseoModerno } from "next/font/google";
+import Confetti, { Rectangle, Circle, Triangle } from '@tholman/confetti';
+
+const museoModerno = MuseoModerno({subsets: ["latin"]})
+
 
 export default function Page() {
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <>
+      {/* TODO: Get rid of the hard-coded margin */}
+      <Confetti total={30} style={{marginTop: "64px"}} Component={[
+        <Rectangle color="#EFF1F3" />,
+        <Circle color="#FED766" />,
+        <Triangle color="#009FB7" />,
+      ]} />
+      <Center>
+        <Stack mt={20}>
+          <Text textStyle="7xl" className={museoModerno.className}>CodeSmell</Text>
+          <Center>
+            <HStack align="center">
+              <Button>Get Started</Button>
+              <Button>Sign In</Button>
+            </HStack>
+          </Center>
+        </Stack>
+      </Center>
+    </>
+   
   )
 }
