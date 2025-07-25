a0863df239ec8ee807e2ece4c05f88b282572340
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-07-25 13:05:01 +0200

    beginning of security
    paswordEncoder, AuthManager and AuthProvider

--- backend/src/main/java/com/codesmelling/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/codesmelling/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.codesmelling.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
