4b2022a31eab85ed9e2f30c55a040115cd76001d
Maksymilian-Katolik <maxkat@student.agh.edu.pl>
2025-07-30 12:37:20 +0200

    Endpoint for returning user progress

--- backend/src/main/java/com/codesmelling/backend/service/ProgressService.java
+++ backend/src/main/java/com/codesmelling/backend/service/ProgressService.java
@@ -4,6 +4,8 @@ import com.codesmelling.backend.config.CsvFileConfigLoader;
 import com.codesmelling.backend.database.tables.AppUser;
 import com.codesmelling.backend.database.tables.Quiz;
 import com.codesmelling.backend.database.tables.UserProgress;
+import com.codesmelling.backend.dto.Progress.UserProgressDto;
+import com.codesmelling.backend.dto.Progress.UserProgressSummaryDto;
 import com.codesmelling.backend.dto.csv.UserProgressCsvDto;
 import com.codesmelling.backend.repository.AppUserRepository;
 import com.codesmelling.backend.repository.QuizRepository;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +28,34 @@ public class ProgressService {
     private final QuizRepository quizRepository;
     private final CsvParserService parser;
     private final CsvFileConfigLoader loader;
+
+
+    public List<UserProgressDto> getProgressForUser(String username) {
+        AppUser user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found: " + username));
+
+        List<UserProgress> progressList = progressRepository.findByUser(user);
+
+        return progressList.stream()
+                .map(p -> UserProgressDto.builder()
+                        .quizId(p.getQuiz().getId())
+                        .quizName(p.getQuiz().getQuizName())// jeśli masz `getName()`
+                        .scorePercent(p.getScorePercent())
+                        .completed(p.isCompleted())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    public UserProgressSummaryDto getUserProgressSummary(String username) {
+        AppUser user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found: " + username));
+
+        List<UserProgress> completed = progressRepository.findByUserAndCompletedTrue(user);
+        long totalQuizzes = quizRepository.count();
+
+        return new UserProgressSummaryDto(completed.size(), (int) totalQuizzes);
+    }
+
 
     public void importProgress() throws IOException {
         InputStream in = loader.load("Progress.csv");
