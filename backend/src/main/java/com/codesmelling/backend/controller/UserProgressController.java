1616c601a7cccaf27cf7379aaedfd7d7573b1ac9
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-08-04 15:40:32 +0200

    hints

--- backend/src/main/java/com/codesmelling/backend/controller/UserProgressController.java
+++ backend/src/main/java/com/codesmelling/backend/controller/UserProgressController.java
@@ -5,6 +5,7 @@ import com.codesmelling.backend.dto.Progress.UserProgressSummaryDto;
 import com.codesmelling.backend.service.ProgressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,13 +18,15 @@ public class UserProgressController {
     private final ProgressService progressService;
 
     @GetMapping("/full")
-    public ResponseEntity<List<UserProgressDto>> getProgress(@RequestParam String username) {
+    public ResponseEntity<List<UserProgressDto>> getProgress(Authentication authentication) {
+        String username = authentication.getName();
         List<UserProgressDto> progress = progressService.getProgressForUser(username);
         return ResponseEntity.ok(progress);
     }
 
     @GetMapping("/summary")
-    public ResponseEntity<UserProgressSummaryDto> getUserProgressSummary(@RequestParam String username) {
+    public ResponseEntity<UserProgressSummaryDto> getUserProgressSummary(Authentication authentication) {
+        String username = authentication.getName();
         return ResponseEntity.ok(progressService.getUserProgressSummary(username));
     }
 }
